69febf690289952c0f68a74b811d65a3e5214e97
Keahuy <orches.kal@gmail.com>
2025-08-28 22:36:33 +0800

    add: 增加了更新提示信封

---
+++ Source/HaulExplicitly/Notice.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace HaulExplicitly;
+
+public class Notice : MapComponent
+{
+    private bool _flag = true;
+
+    public Notice(Map map) : base(map)
+    {
+    }
+
+    public override void MapComponentTick()
+    {
+        if (!_flag) return;
+        string text = "HaulExplicitly.UpdateNotice.title".Translate() + "\n\n" + "HaulExplicitly.UpdateNotice.description".Translate() + "\n2025.8.28 22:00(CST)";
+        ChoiceLetter choiceLetter = LetterMaker.MakeLetter("HaulExplicitly.UpdateNotice.LetterTitle".Translate(), text, LetterDefOf.PositiveEvent);
+        Find.LetterStack.ReceiveLetter(choiceLetter);
+        Find.Archive.Remove(choiceLetter);
+        _flag = false;
+    }
+}
